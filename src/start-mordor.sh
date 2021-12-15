9e1ae6de5b09b1356ae23074069d6b8289f5f4f7
Leo Falco <leonardo.falco@outlook.com>
2021-12-15 10:58:20 -0300

    feat: start-servers

---
+++ src/start-mordor.sh
@@ -0,0 +1,11 @@
+fuser -n tcp -k 8888 || true
+fuser -n tcp -k 8000 || true
+fuser -n tcp -k 4000 || true
+fuser -n tcp -k 3000 || true
+fuser -n tcp -k 3334 || true
+
+npx concurrently \
+"cd ~/FieldControl/eyeofsauron && . ~/.nvm/nvm.sh && nvm use && npm run start-dev" \
+"cd ~/FieldControl/mordor && . ~/.nvm/nvm.sh && nvm use && npm run start-dev" \
+"cd ~/FieldControl/blackgate/api && . ~/.nvm/nvm.sh && nvm use && npm run start-dev" \
+"cd ~/FieldControl/eyeofsauron-2 && . ~/.nvm/nvm.sh && nvm use && npm run start-dev"
