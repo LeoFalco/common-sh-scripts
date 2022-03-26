be12725d865a5ae4063eb3b07233b7c040d7ece8
Leo Falco <leonardo.falco@outlook.com>
2022-03-26 08:00:07 -0300

    feat: check if pr already exists

--- src/pr-create.js
+++ src/pr-create.js
@@ -2,11 +2,19 @@
 
 import 'zx/globals'
 
-const branch = await $`git rev-parse --abbrev-ref HEAD`.then(out => out.stdout.trim())
+const existingPrUrl = await $`gh pr view --json url --jq .url`.then(parseOutput).catch(() => null)
+
+if(existingPrUrl){
+  await open(existingPrUrl)
+
+  process.exit(0)
+}
+
+const branch = await $`git rev-parse --abbrev-ref HEAD`.then(parseOutput)
 
 await $`git push -uf origin ${branch} --no-verify`
 
-const repoPath= await $`git rev-parse --show-toplevel`.then(out => out.stdout.trim())
+const repoPath= await $`git rev-parse --show-toplevel`.then(parseOutput)
 
 const workflowFieldnewsPath = repoPath + '/.github/workflows/fieldnews.yml'
 
@@ -18,10 +26,10 @@ if(fs.existsSync(repoPath + '/.github/pull_request_template.md')){
 let prTemplate = prTemplatePath && fs.readFileSync(prTemplatePath, {  encoding: 'utf8' })
 
 const prefix = fs.existsSync(workflowFieldnewsPath)
-  ?  await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(out => out.stdout.trim())
+  ?  await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(parseOutput)
   : ''
 
-const commitMessage= await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(out => out.stdout.trim())
+const commitMessage= await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(parseOutput)
 
 const title = `${prefix} ${commitMessage}`.trim()
 
@@ -37,11 +45,19 @@ const reviewers = await $`gh api orgs/FieldControl/members --jq '.[].login' | xa
 
 await $`gh pr create --assignee @me --draft --title ${title} --body ${prTemplate} --reviewer ${reviewers}`
 
-const url = await $`gh pr view --json url --jq .url`.then(out => out.stdout.trim())
+const url = await $`gh pr view --json url --jq .url`.then(parseOutput)
 
-$`google-chrome ${url}`
+await open(url)
 
 function issueNumberFromBranch(branch) {
   const match = branch.match(/^(\d+)/)
   return match && match[0]
+}
+
+function parseOutput(out) {
+  return out.stdout.trim()
+}
+
+async function open(url) {
+  await $`google-chrome ${url}`
 }
