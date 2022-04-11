214ca1241b90c669846c1704244e7a6479d1ad63
Leo Falco <leonardo.falco@outlook.com>
2022-04-11 18:30:54 -0300

    fix: corrige chamada a funcao prepend

--- src/pr-create.js
+++ src/pr-create.js
@@ -1,34 +1,33 @@
 #!/usr/bin/env zx
 
 import 'zx/globals'
+import chalk from 'chalk';
+import lodash from 'lodash';
 
-const existingPrUrl = await $`gh pr view --json url --jq .url`.then(parseOutput).catch(() => null)
+const { yellow, green } = chalk;
+const { chain } = lodash
 
-if(existingPrUrl){
-  await open(existingPrUrl)
-  process.exit(0)
-}
-
-const branch = await $`git rev-parse --abbrev-ref HEAD`.then(parseOutput)
+console.log()
+const branch = await $`git rev-parse --abbrev-ref HEAD`.then(getOutputAsString)
+console.log()
 
 await $`git push -uf origin ${branch} --no-verify`
+console.log()
 
-const repoPath= await $`git rev-parse --show-toplevel`.then(parseOutput)
+const repoPath = await $`git rev-parse --show-toplevel`.then(getOutputAsString)
+
+const prTemplatePath = repoPath + '/.github/pull_request_template.md'
+let prTemplate = fs.existsSync(prTemplatePath)
+? fs.readFileSync(prTemplatePath, {  encoding: 'utf8' })
+: ''
+
+const commitMessage= await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(getOutputAsString)
 
 const workflowFieldnewsPath = repoPath + '/.github/workflows/fieldnews.yml'
-
-let prTemplatePath = ''
-if(fs.existsSync(repoPath + '/.github/pull_request_template.md')){
-  prTemplatePath =  repoPath + '/.github/pull_request_template.md'
-}
-
-let prTemplate = prTemplatePath && fs.readFileSync(prTemplatePath, {  encoding: 'utf8' })
-
 const prefix = fs.existsSync(workflowFieldnewsPath)
-  ?  await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(parseOutput)
+  ?  await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(getOutputAsString)
   : ''
 
-const commitMessage= await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(parseOutput)
 
 const title = `${prefix} ${commitMessage}`.trim()
 
@@ -38,23 +37,58 @@ if(issueNumber) {
   prTemplate = `- closes #${issueNumber}\n\n` + prTemplate
 }
 
-prTemplate = prTemplate || 'empty'
+if(!prTemplate){
+  prTemplate = `empty`
+}
 
-const reviewers = await $`gh api orgs/FieldControl/members --jq '.[].login' | xargs`.then(out => out.stdout.trim().replace(/\s+/g, ','))
+const allReviewersAsString = await $`gh api orgs/FieldControl/members --jq '.[].login' | xargs`.then(out => out.stdout.trim().replace(/\s+/g, ','))
 
-await $`gh pr create --assignee @me --draft --title ${title} --body ${prTemplate} --reviewer ${reviewers}`
+console.log()
 
-const url = await $`gh pr view --json url --jq .url`.then(parseOutput)
 
-await open(url)
+// const sammathnaurTeam = {
+//   name: 'sammathnaur',
+//   members: [
+//     'LeoFalco',
+//     'ThayDias',
+//     'willaug'
+//   ]
+// }
+// const reviewers = chain(allReviewersAsString)
+//   .split(',')
+//   .sort()
+//   .reject(nickname => sammathnaurTeam.members.includes(nickname))
+//   .value()
+
+
+const reviewers = [
+  'FieldControl/sammathnaur',
+  'FieldControl/valar',
+  'Giovani-f',
+  'IgorMoraes15',
+  'LeoFalco',
+  'caiorsantanna',
+  'godinhojoao',
+  'lfreneda',
+  'thalescrosa',
+  'victorreinor'
+]
+
+console.log('reviewers', reviewers)
+
+await (quiet($`gh pr create --assignee @me --draft --title ${title} --body ${prTemplate} --reviewer ${reviewers.join(',')}`))
+
+const url = await $`gh pr view --json url --jq .url`.then(getOutputAsString)
+console.log()
+console.log(`pr opened ${url}`)
 
 function issueNumberFromBranch(branch) {
   const match = branch.match(/^(\d+)/)
   return match && match[0]
 }
 
-function parseOutput(out) {
-  return out.stdout.trim()
+function getOutputAsString(process) {
+  return process && process.stdout.trim()
 }
 
 async function open(url) {
