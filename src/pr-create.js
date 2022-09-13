9fe3990998aa484fbe7c213f308ca3bd7c4d66e6
Leo Falco <leonardo.falco@outlook.com>
2022-09-12 21:20:02 -0300

    feat: atualiza nickname dos devs

--- src/pr-create.js
+++ src/pr-create.js
@@ -18,14 +18,14 @@ const repoPath = await $`git rev-parse --show-toplevel`.then(parseOutput)
 
 const prTemplatePath = repoPath + '/.github/pull_request_template.md'
 let prTemplate = fs.existsSync(prTemplatePath)
-? fs.readFileSync(prTemplatePath, {  encoding: 'utf8' })
-: ''
+  ? fs.readFileSync(prTemplatePath, { encoding: 'utf8' })
+  : ''
 
-const commitMessage= await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(parseOutput)
+const commitMessage = await $`git log --pretty=%B -n 1 | cut -d ':' -f 2 | xargs`.then(parseOutput)
 
 const workflowFieldnewsPath = repoPath + '/.github/workflows/fieldnews.yml'
 let prefix = fs.existsSync(workflowFieldnewsPath)
-  ?  await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(parseOutput)
+  ? await $`cat ${workflowFieldnewsPath} | grep allowed_prefix | cut -d ':' -f 2 | sed "s/'//g" | xargs`.then(parseOutput)
   : ''
 
 prefix = prefix.split(',')[0].trim()
@@ -35,11 +35,11 @@ const title = `${prefix} ${capitalize(commitMessage)}`.trim()
 
 const issueNumber = issueNumberFromBranch(branch)
 
-if(issueNumber) {
+if (issueNumber) {
   prTemplate = `- closes #${issueNumber}\n\n` + prTemplate
 }
 
-if(!prTemplate){
+if (!prTemplate) {
   prTemplate = `empty`
 }
 
@@ -48,20 +48,30 @@ const allReviewersAsString = await $`gh api orgs/FieldControl/members --jq '.[].
 console.log()
 
 
+
 const reviewers = [
   'FieldControl/Enterprise',
   'FieldControl/Fieldevelopers',
-  'Giovani-f',
-  'IgorMoraes15',
-  'LeoFalco',
+  'Arcaino',
+  'brunohforcato',
   'caiorsantanna',
+  'camargobiel',
+  'Carlos-F-Braga',
+  'gilmarferrini',
   'godinhojoao',
-  'lfreneda',
-  'victorreinor',
+  'guilhermeviiniidev',
+  'GutoRomagnolo',
+  'helderlim',
+  'jbrandao',
+  'joaovictorlongo',
+  'kweripx',
   'LeoFalco',
-  'ThayDias',
+  'lfreneda',
+  'ottonielmatheus',
+  'satakedev',
+  'sousxa',
+  'victorreinor',
   'willaug',
-  'satakedev'
 ]
 
 console.log('reviewers', reviewers)
@@ -72,19 +82,19 @@ const url = await $`gh pr view --json url --jq .url`.then(parseOutput)
 console.log()
 console.log(`pr opened ${url}`)
 
-function issueNumberFromBranch(branch) {
+function issueNumberFromBranch (branch) {
   const match = branch.match(/^(\d+)/)
   return match && match[0]
 }
 
-function parseOutput(process) {
+function parseOutput (process) {
   return process && process.stdout.trim()
 }
 
-async function open(url) {
+async function open (url) {
   await $`google-chrome ${url}`
 }
 
-function capitalize(text){
+function capitalize (text) {
   return text.charAt(0).toUpperCase() + text.slice(1)
 }
